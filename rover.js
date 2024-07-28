cd9b4becc191a12a7052d9b873580e8325179d7e
Adam Stanislawsky <adamstanislawsky@Adams-MBP.attlocal.net>
2024-07-27 21:26:54 -0500

    test 8 is passing, likely false positive

--- rover.js
+++ rover.js
@@ -1,19 +1,19 @@
 class Rover {
    // Write code here!
-   constructor(position, mode, generatorWatts) {
+   constructor(position) {
       this.position = Number(position); //will be a number representing the rover's position
       // if (!position) {
       //   throw Error("Position is required.");
       // }
       this.mode = 'NORMAL';
       this.generatorWatts = 110;
-      
-      // receiveMessage(message){
-
-      // }
 
     }
 
+   receiveMessage(message){
+
+      return message;
+   }
 
 
 
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -23,26 +23,35 @@ describe("Rover class", function() {
 
 
   // TEST 8
+// “response returned by receiveMessage contains the name of the message”
+
+test("response returned by receiveMessage contains the name of the message", function() {
+  let testMessage = new Message ("aMessage")
+  let testRover = new Rover (1234)
+  let response = testRover.receiveMessage(testMessage)
+  expect(response.name).toEqual("aMessage");
 
 
+});
 
 
 
   // TEST 9
-
+//“response returned by receiveMessage includes two results if two commands are sent in the message”
 
 
 
 
   // TEST 10
-
-
+//“responds correctly to the status check command”
 
 
 
   // TEST 11
+//“responds correctly to the mode change command”
 
-
+//1. The test should check the completed property and rover mode for accuracy.
+//2. The rover has two modes that can be passed as values to a mode change command: ‘LOW_POWER’ and ‘NORMAL’.
 
 
 
